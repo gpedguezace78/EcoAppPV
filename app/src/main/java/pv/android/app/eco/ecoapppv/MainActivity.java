b24a2a0bda4db42d0895e91ac4e0aaf28852c39e
Guadalupe Dguez A <gpedguezace78@gmail.com>
2018-05-13 17:21:58 -0500

    Agregar Activity de Ventas

--- app/src/main/java/pv/android/app/eco/ecoapppv/MainActivity.java
+++ app/src/main/java/pv/android/app/eco/ecoapppv/MainActivity.java
@@ -1,13 +1,33 @@
 package pv.android.app.eco.ecoapppv;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btnAbrirVta;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAbrirVta = (Button) findViewById(R.id.BtnIngresar);
+        btnAbrirVta.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.BtnIngresar:
+                Intent intentvta = new Intent(this, VentasActivity.class);
+                startActivity(intentvta);
+                break;
+            default:
+                break;
+        }
     }
 }
